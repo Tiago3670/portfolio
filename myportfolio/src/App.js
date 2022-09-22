57d12895f89a9336841d95c9f71cad7b96de43a4
Tiago3670 <56228294+Tiago3670@users.noreply.github.com>
2022-09-22 14:12:09 +0100

    nada

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function ()
+{
+  const axios = require('axios');
+  const [ligas,setLigas]=useState("");
+  var i=1;
+  useEffect(() => {
+    teste();
+  });
+
+
+  function teste()
+  {
+    axios.get('')
+    .then(response => setLigas(response.data));
+     console.log(ligas);
+  }
+
   return (
-  <></>
-  );
+    <>
+        <img src='https://picsum.photos/id/+{$i}+/400/500' ></img>
+    </>
+    );
 }
+ 
 
-export default App;
+  
